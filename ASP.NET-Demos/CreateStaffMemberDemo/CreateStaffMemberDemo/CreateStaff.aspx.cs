7b40eb41a4e026a8da063a066bb148f9612885c2
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-03-20 19:54:11 +0000

    drop down role selector created

--- ASP.NET-Demos/CreateStaffMemberDemo/CreateStaffMemberDemo/CreateStaff.aspx.cs
+++ ASP.NET-Demos/CreateStaffMemberDemo/CreateStaffMemberDemo/CreateStaff.aspx.cs
@@ -10,8 +10,10 @@ using CreateStaffMemberDemo;
 public partial class CreateStaff : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
-    {   
-
+    {
+        this.ChooseRole.DataSource = ListRoles();
+        this.ChooseRole.DataTextField = "Description";
+        this.ChooseRole.DataBind();
     }
 
     // Button click event to submit user data
@@ -26,9 +28,15 @@ public partial class CreateStaff : System.Web.UI.Page
     }
 
     // Method to display volunteer roles
-    protected void ListRoles()
+    protected List<Role> ListRoles()
     {
+        List<Role> possibleRoles = new List<Role>();
 
+        possibleRoles.Add(new Role { Description = "Head Librarian"});
+        possibleRoles.Add(new Role { Description = "Chief Librarian" });
+        possibleRoles.Add(new Role { Description = "Check Out Librarian" });
+
+        return possibleRoles;
     }
 
     // Method to create a new staff member (write file to JSON)
@@ -39,8 +47,8 @@ public partial class CreateStaff : System.Web.UI.Page
     }
 
     // Method to check if data being input already exists
-    protected void CheckStaffExists()
+    protected bool CheckStaffExists(Staff staffMember)
     {
-
+        return true;
     }
 }
