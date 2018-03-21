e371e8d7611efd4d20e85a65ac4ff0ed8b15a461
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-03-21 10:09:17 +0000

    custom coded color selector

--- jQuery-Demos/jQuery-Demo-1/javascript/script.js
+++ jQuery-Demos/jQuery-Demo-1/javascript/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
+    // make images draggable
     $('.image').draggable();
+
+    // give each image a random color
+    var colors = ["red", "blue", "green", "yellow", "orange"];
+    var images = [$('#top-left'), $('#top-mid'), $('#top-right'), $('#bottom-left'), $('#bottom-mid'), $('#bottom-right')];
+
+    for (let i = 0; i < images.length; i++) {
+        images[i].css('backgroundColor', colors[Math.floor(Math.random() * colors.length)]);
+    }
 });
